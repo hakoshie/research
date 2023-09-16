d51bbafeabfc3066c09b46d7654b939c09ab3279
hakoshie <hakoshie@gmail.com>
2023-09-17 07:54:53 +0900

    imputed with rf and computed metrics for each estimation methods

--- estimate/event/plot_result.py
+++ estimate/event/plot_result.py
@@ -6,8 +6,8 @@ from linearmodels.panel import PanelOLS, PooledOLS, RandomEffects, compare
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_result(result, start_from=1, period=17, insert_index=6, time_start=-7,ylim=(-1,1),plot_type="o-",file_name="result", title="Event Study Coefficients with Confidence Intervals",markersize=5):
-    colors=sns.color_palette("husl", 4)
+def plot_result(result, start_from=1, period=17, insert_index=6, time_start=-7,ylim=(-1,1),plot_type="o-",file_name="result", title="Event Study Coefficients with Confidence Intervals",markersize=5,color_palette="Paired"):
+    colors=sns.color_palette(color_palette, 8)
     summary_table = pd.DataFrame(result.summary.tables[1].data[start_from:start_from + period], columns=result.summary.tables[1].data[0])
     new_row = [0] * len(summary_table.columns)
     summary_table = pd.concat([summary_table.iloc[:insert_index], pd.DataFrame([new_row], columns=summary_table.columns), summary_table.iloc[insert_index:]]).reset_index(drop=True)
@@ -17,10 +17,10 @@ def plot_result(result, start_from=1, period=17, insert_index=6, time_start=-7,y
     plt.figure(figsize=(10, 6))
 
     # Plot coefficients as points
-    plt.plot(summary_table['Time'], summary_table["Parameter"], plot_type, label='Coefficients', color=colors[0],markersize=markersize)
+    plt.plot(summary_table['Time'], summary_table["Parameter"], plot_type, label='Coefficients', color=colors[1],markersize=markersize)
 
     # Plot confidence intervals as error bars
-    plt.fill_between(summary_table['Time'], summary_table['Lower CI'], summary_table['Upper CI'], color=colors[1], alpha=0.3, label='Confidence Intervals')
+    plt.fill_between(summary_table['Time'], summary_table['Lower CI'], summary_table['Upper CI'], color=colors[0], alpha=0.3, label='Confidence Intervals')
 
     # Separate the data for regression before and after insert_index
     pre_insert_data = summary_table[summary_table['Time'] <= insert_index+time_start]
@@ -40,8 +40,8 @@ def plot_result(result, start_from=1, period=17, insert_index=6, time_start=-7,y
     model_post = sm.OLS(post_insert_data['Parameter'], X_post).fit()
 
     # Plot regression lines
-    plt.plot(pre_insert_data['Time'], model_pre.predict(X_pre), '--', label='Regression Line (Before Event)', color=colors[2])
-    plt.plot(post_insert_data['Time'], model_post.predict(X_post), '--', label='Regression Line (After Event)', color=colors[3])
+    plt.plot(pre_insert_data['Time'], model_pre.predict(X_pre), '--', label='Regression Line (Before Event)', color=colors[3])
+    plt.plot(post_insert_data['Time'], model_post.predict(X_post), '--', label='Regression Line (After Event)', color=colors[5])
 
     plt.xlabel('Time')
     plt.ylim(ylim)
